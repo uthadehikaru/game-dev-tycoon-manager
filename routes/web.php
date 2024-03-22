16c5ec9210e5d3d452b289b499f6b995777752db
zuhriutama <zuhriutama@gmail.com>
2024-03-22 16:19:09 +0700

    bug fix on route

--- routes/web.php
+++ routes/web.php
@@ -4,10 +4,11 @@ use App\Http\Controllers\RegisterController;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
-Route::view('/', 'welcome');
-Route::view('/register', 'register');
+Route::view('/', 'welcome')->name('home');
+Route::redirect('/login', 'admin/login')->name('login');
+Route::view('/register', 'register')->name('register');
 Route::post('/register', RegisterController::class);
 Route::get('/logout', function() {
     Auth::logout();
     return redirect('/');
-});
+})->name('logout');
