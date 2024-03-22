8155c866f8138ac76b3ffd5a296d0c5a8cbb048b
zuhriutama <zuhriutama@gmail.com>
2024-03-22 16:15:45 +0700

    add logout

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,13 @@
 <?php
 
 use App\Http\Controllers\RegisterController;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 Route::view('/', 'welcome');
 Route::view('/register', 'register');
 Route::post('/register', RegisterController::class);
+Route::get('/logout', function() {
+    Auth::logout();
+    return redirect('/login');
+});
